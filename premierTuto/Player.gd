5ad0835791956982b90fed7938e5eecdf29a5d75
Roulalou <sebastien.gios@student.uclouvain.be>
2022-04-02 17:28:51 +0200

    Update Player.gd

--- premierTuto/Player.gd
+++ premierTuto/Player.gd
@@ -2,9 +2,10 @@ extends KinematicBody2D
 
 const UP = Vector2(0, -1)
 const GRAVITY = 20
-const MAXFALLSPEED = 700
-const MAXSPEED = 300
-const JUMPFORCE = 500
+const MAXFALLSPEED = 500
+const MAXSPEED = 160
+const JUMPFORCE = 400
+const ACCEL = 20
 
 
 var motion = Vector2()
@@ -15,14 +16,16 @@ func _physics_process(delta):
 	
 	motion.y += GRAVITY
 	if motion.y > MAXFALLSPEED:
-		motion.y = MAXFALLSPEED
+		motion.y = MAXFALLSPEED 
+		
+	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
 	
 	if Input.is_action_pressed("right"):
-		motion.x = MAXSPEED
+		motion.x += ACCEL
 	elif Input.is_action_pressed("left"):
-		motion.x = -MAXSPEED
+		motion.x -= ACCEL
 	else:
-		motion.x = 0
+		motion.x = lerp(motion.x, 0, 0.2) #acceleration du perso
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
