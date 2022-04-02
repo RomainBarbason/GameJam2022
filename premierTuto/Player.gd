9332c5c226dcc8b8f89593ccebbe21a957e319b3
Roulalou <sebastien.gios@student.uclouvain.be>
2022-04-02 18:02:51 +0200

    animated player

--- premierTuto/Player.gd
+++ premierTuto/Player.gd
@@ -9,6 +9,8 @@ const ACCEL = 20
 
 
 var motion = Vector2()
+var facing_right = true
+
 func _ready():
 	pass # Replace with function body.
 
@@ -18,17 +20,34 @@ func _physics_process(delta):
 	if motion.y > MAXFALLSPEED:
 		motion.y = MAXFALLSPEED 
 		
+	if facing_right == true:
+		$Sprite.scale.x = 1
+	else:
+		$Sprite.scale.x = -1
+	
 	motion.x = clamp(motion.x, -MAXSPEED, MAXSPEED)
 	
 	if Input.is_action_pressed("right"):
 		motion.x += ACCEL
+		facing_right = true
+		$AnimationPlayer.play("Running")
 	elif Input.is_action_pressed("left"):
 		motion.x -= ACCEL
+		facing_right = false
+		$AnimationPlayer.play("Running")
 	else:
 		motion.x = lerp(motion.x, 0, 0.2) #acceleration du perso
+		$AnimationPlayer.play("Idle")
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			motion.y = -JUMPFORCE
+			
+	if !is_on_floor():
+		if motion.y < 0:
+			$AnimationPlayer.play("Jump")
+		elif motion.y > 0:
+			$AnimationPlayer.play("Falling")
+		
 
 	motion = move_and_slide(motion,UP)
